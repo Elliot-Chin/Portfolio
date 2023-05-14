2b35b7cba79dcd7a0571211a82fa7df2fee995e2
Elliot-Chin <ychin@unb.ca>
2023-05-14 01:34:49 -0300

    added google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,23 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300&display=swap" rel="stylesheet" />
-        <link rel="icon" type='image/jpg' href="./images/EC.jpg"/>
+        <link rel="icon" type='image/jpg' href="./images/EC.jpg" />
+
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0MH67YDFQN" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-0MH67YDFQN');
+          `
+          }} />
+
+
+
       </Head>
       <body>
         <Main />
